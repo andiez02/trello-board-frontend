55ba9f7daecdec4a2985437f575d6eb8f6cbb9c9
andiez02 <146305529+andiez02@users.noreply.github.com>
2025-01-01 23:36:50 +0700

    Revert "Feature: Add Board Bar content (column and card)"

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,11 @@
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
-
-const APP_BAR_HEIGHT = "58px";
-const BOARD_BAR_HEIGHT = "60px";
-const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`;
+// import { cyan, deepOrange, orange, teal } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = extendTheme({
   trello: {
-    appBarHeight: APP_BAR_HEIGHT,
-    boardBarHeight: BOARD_BAR_HEIGHT,
-    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    appBarHeight: "58px",
+    boardBarHeight: "60px",
   },
   colorSchemes: {
     // light: {
@@ -61,15 +57,6 @@ const theme = extendTheme({
           color: theme.palette.primary.main,
           fontSize: "0.875rem",
         }),
-      },
-    },
-    MuiTypography: {
-      styleOverrides: {
-        root: {
-          "&.MuiTypography-body1": {
-            fontSize: "0.875rem",
-          },
-        },
       },
     },
     MuiOutlinedInput: {
